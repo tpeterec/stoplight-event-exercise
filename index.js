f3f9449000907dabd89a27b42e544b917dd6bba5
tpeterec <alex1996p3@gmail.com>
2023-03-03 15:00:33 -0900

    Finalized stop light project

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-(function() {
-  'use strict';
+let stopButton = document.getElementById("stopButton");
+let redLight = document.getElementById("stopLight");
+stopButton.addEventListener("click", function () {
+  redLight.classList.toggle("stop");
+});
 
-  // YOUR CODE HERE
-})();
+let slowButton = document.getElementById("slowButton");
+let yellowLight = document.getElementById("slowLight");
+slowButton.addEventListener("click", function () {
+  yellowLight.classList.toggle("slow");
+});
+
+let goButton = document.getElementById("goButton");
+let greenLight = document.getElementById("goLight");
+goButton.addEventListener("click", function () {
+  greenLight.classList.toggle("go");
+});
+
+let buttons = document.querySelectorAll(".button");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("mouseenter", function (event) {
+    buttonLog(event.target.innerText);
+  });
+}
+
+goButton.addEventListener("mouseenter", function () {
+  goLight.classList.toggle("go");
+});
+goButton.addEventListener("mouseleave", function () {
+  goLight.classList.toggle("go");
+});
+
+slowButton.addEventListener("mouseenter", function () {
+  slowLight.classList.toggle("slow");
+});
+slowButton.addEventListener("mouseleave", function () {
+  slowLight.classList.toggle("slow");
+});
+
+stopButton.addEventListener("mouseenter", function () {
+  stopLight.classList.toggle("stop");
+});
+stopButton.addEventListener("mouseleave", function () {
+  stopLight.classList.toggle("stop");
+});
+
+let leftButton = buttons.forEach((button) => {
+  button.addEventListener("mouseleave", (event) => {
+    buttonLogOut(event.target.textContent);
+  });
+});
+
+function buttonLog(buttonName) {
+  console.log(`Enter ${buttonName} button`);
+}
+
+function buttonLogOut(buttonName) {
+  console.log(`Left ${buttonName} button`);
+}
